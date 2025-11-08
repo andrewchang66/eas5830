0a9611dae1e231060b32d1c68ffdaf0fc8715683
andrewchang66 <121365293+andrewchang66@users.noreply.github.com>
2025-11-08 22:03:19 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -22,40 +22,51 @@ contract Destination is AccessControl {
         _grantRole(WARDEN_ROLE, admin);
     }
 
+
+
+
+
+
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
-	    address _wrapped = wrapped[_underlying];
-	    require(_wrapped != address(0), "token not registered");
-	    require(_to != address(0), "bad recipient");
-	    require(_amount > 0, "zero amount");
-	
-	    BridgeToken(_wrapped).mint(_to, _amount);
-	    emit Wrap(_underlying, _to, _amount);
+    address w = wrapped_tokens[_underlying_token];
+    require(w != address(0), "token not registered");
+    require(_recipient != address(0), "bad recipient");
+    require(_amount > 0, "zero amount");
+
+    BridgeToken(w).mint(_recipient, _amount);
+    emit Wrap(_underlying_token, w, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-	    require(_wrappedToken != address(0), "bad wrapped token");
-	    require(_recipientOnSource != address(0), "bad source recipient");
-	    require(_amount > 0, "zero amount");
-	
-	    BridgeToken(_wrappedToken).burnFrom(msg.sender, _amount);
-	
-	    emit Unwrap(_wrappedToken, _recipientOnSource, _amount);
+    require(_wrapped_token != address(0), "bad wrapped token");
+    address u = underlying_tokens[_wrapped_token];
+    require(u != address(0), "unknown wrapped token");
+    require(_recipient != address(0), "bad source recipient");
+    require(_amount > 0, "zero amount");
+
+    // If your BridgeToken uses a different burn method (e.g., bridgeBurnFrom),
+    // change the next line accordingly.
+    BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+
+    emit Unwrap(u, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-    	require(_underlying != address(0), "invalid underlying");
-    	require(wrapped[_underlying] == address(0), "already created");
+    require(_underlying_token != address(0), "invalid underlying");
+    require(wrapped_tokens[_underlying_token] == address(0), "already created");
 
-    	BridgeToken token = new BridgeToken(_name, _symbol);
-    	address _wrapped = address(token);
+    // BridgeToken constructor: (address _underlying, string memory name, string memory symbol, address admin)
+    BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
+    address w = address(token);
 
-    	wrapped[_underlying] = _wrapped;
-    	emit Creation(_underlying, _wrapped);
-    	return _wrapped;
+    wrapped_tokens[_underlying_token] = w;
+    underlying_tokens[w] = _underlying_token;
+    tokens.push(w);
 
+    emit Creation(_underlying_token, w);
+    return w;
 	}
 
 }
